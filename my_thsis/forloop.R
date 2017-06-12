469371eb8fc62c225534d18afd57887e4f217644
MichaelXiao <y19938222@gmail.com>
2017-06-13 00:46:18 +0800

    把產生結果寫成data frame, 並且可以進行shuffle

--- my_thsis/forloop.R
+++ my_thsis/forloop.R
@@ -1,14 +1,18 @@
 count = 0
+N=0
+dfrm <- data.frame(coli=numeric(N),colj=numeric(N))
 
 for (i in 1:6){ 
+  print(i)
   for (j in i+1:7){
     if(j > 7) next
+    v <- list(i,j)
     cat("(",i,",",j, ")")
-    count = count + 1
-    
-    CSTDF <- data.frame(id = count, coli = i, colj = j)
+   # dfrm <- do.call(rbind, v)
+    count = count +1
+    newrow <- data.frame(coli = i, colj = j)
+    dfrm <- rbind(dfrm, newrow)
   }
-  print(i)
 }
 
-CSTDF
+df2 <- dfrm[sample(nrow(dfrm)),] # shuffle
